352417cf8f309df5fd010a79882b9362b1e3c658
Acortezm <anabel.cortezm@gmail.com>
2024-09-11 09:15:50 -0500

    CRUD working

--- Blog web application/index.js
+++ Blog web application/index.js
@@ -1,6 +1,5 @@
 import express from "express"
 import bodyParser from "body-parser"
-import { render } from "ejs";
 
 const app = express()
 const port = 3000
@@ -11,23 +10,51 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {entries : entries})
-    console.log("get/")
 })
 
-app.get("/submit", (req, res) => {
-    res.render("submit.ejs")
+app.get("/entries", (req, res) => {
+    res.render("entries.ejs")
 })
 
+//saving the form data into an array
 app.post("/", (req, res) => {
     entries.push(req.body)
-    res.render("submit.ejs")
-    console.log(req.body, entries)
+    res.redirect("/")
 
 })
 
+//we received url with the index of the data in the array so we can fetch it and send it. 
 app.get("/entries/:id", (req, res)=>{
-    res.send("working on it")
+    const index = parseInt(req.params.id)
+    const entryObj = entries[index]
+    res.render("entries.ejs",  {
+        index : index,
+        entries : entryObj
+    })
+    console.log(entryObj)
 })
+
+//to edit the entry in the array
+app.post("/entries/:id", (req, res)=>{
+    const index = parseInt(req.params.id);
+    const updatedEntry = {
+        title: req.body.title,
+        content: req.body.content
+    };
+    entries[index] = updatedEntry
+    res.redirect("/")
+
+})
+
+app.post("/entries/:id", (req, res)=>{
+    const index = parseInt(req.params.id);
+    entries.splice(index, 1)
+    res.redirect("/")
+
+})
+
+
+
 
 
 app.listen(port, () =>{
