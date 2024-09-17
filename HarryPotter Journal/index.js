8c1fd0b63d76d343d69253d3297a7c92027fe523
Acortezm <anabel.cortezm@gmail.com>
2024-09-17 10:37:18 -0500

    Add basic css and devisions

--- HarryPotter Journal/index.js
+++ HarryPotter Journal/index.js
@@ -8,11 +8,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
 const port = 3000
 
-app.use(express.static("/public"))
+app.use(express.static("public"))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-    res.send("Hi")
+    res.render("index.ejs")
 })
 
 app.listen(3000, ()=>{
