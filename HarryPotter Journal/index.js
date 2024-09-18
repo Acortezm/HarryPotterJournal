2194ed8e75f97d53efe30a4fe6c09699574eb050
Acortezm <anabel.cortezm@gmail.com>
2024-09-18 08:56:31 -0500

    chapters are showing for each book. Needs styling but it's  working

--- HarryPotter Journal/index.js
+++ HarryPotter Journal/index.js
@@ -10,9 +10,9 @@ const port = 3000
 const apiUrl = "https://api.potterdb.com/v1/"
 
 app.use(express.static("public"))
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//TO RENDER ALL THE BOOKS
 app.get("/", async (req, res) => {
     try{
         const response = await axios.get(apiUrl + "/books")
@@ -24,14 +24,18 @@ app.get("/", async (req, res) => {
     }
 })
 
-app.get("/chapters", async (req, res) => {
+//TO RENDER THE CHAPTERS WITHIN A BOOK
+app.get("/chapters/:index", async (req, res) => {
+    const bookId = req.query.bookId //array of all book IDs
+    const index = parseInt(req.params.index) //the index of the book that was clicked
     try{
-        const response = await axios.get(apiUrl + "/books")
-        res.render("index.ejs", {
-            books : response.data.data
+        const response = await axios.get(apiUrl + "/books/" + bookId[index] + "/chapters")
+        res.render("chapters.ejs", {
+            chapters : response.data.data //array of all the chapters. 
         })
+        
     }catch(error){
-        console.log("failed request: ", error)
+        console.log("failed request: ", error.message)
     }
 })
 
