890691eb232493d4d15ed6c01d2a7da29f2da4b0
Acortezm <anabel.cortezm@gmail.com>
2024-09-18 07:07:53 -0500

    Add books cover, book title, book content and responsiveness

--- HarryPotter Journal/index.js
+++ HarryPotter Journal/index.js
@@ -7,15 +7,34 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
 const port = 3000
+const apiUrl = "https://api.potterdb.com/v1/"
 
 app.use(express.static("public"))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.render("index.ejs")
+app.get("/", async (req, res) => {
+    try{
+        const response = await axios.get(apiUrl + "/books")
+        res.render("index.ejs", {
+            books : response.data.data
+        })
+    }catch(error){
+        console.log("failed request: ", error)
+    }
 })
 
-app.listen(3000, ()=>{
+app.get("/chapters", async (req, res) => {
+    try{
+        const response = await axios.get(apiUrl + "/books")
+        res.render("index.ejs", {
+            books : response.data.data
+        })
+    }catch(error){
+        console.log("failed request: ", error)
+    }
+})
+
+app.listen(port, ()=>{
     console.log("Server is running in port 3000")
 })
